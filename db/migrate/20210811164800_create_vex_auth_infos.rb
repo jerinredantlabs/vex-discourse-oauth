d96294576b8ccb05524c262f66908d65fe612fe9
Jerin Kuriakose <jerin@redantlabs.io>
2022-03-17 17:42:03 +0530

    uncommented index migrations - force delete before create table

--- db/migrate/20210811164800_create_vex_auth_infos.rb
+++ db/migrate/20210811164800_create_vex_auth_infos.rb
@@ -1,6 +1,6 @@
 class CreateVexAuthInfos < ActiveRecord::Migration[6.1]
   def change
-    create_table :vex_auth_infos, if_not_exists: true do |t|
+    create_table :vex_auth_infos, force: true do |t|
       t.bigint :vex_id
       t.integer :user_id
       t.string :email
